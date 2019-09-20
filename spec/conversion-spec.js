331aed54f785f9469956cceddc2da7980e6c2a72
Student <example@example.com>
2019-09-20 13:21:06 -0700

    add conversion constructor

--- spec/conversion-spec.js
+++ spec/conversion-spec.js
@@ -2,8 +2,8 @@ import { weightConversion } from './../src/conversion';
 
 describe('weightConversion', function () {
   
-  it('should test for the value of gramWeight', function() {
-    let conversion = new weightConversion (45359.2)
-    expect(conversion.gramWeight).toEqual(45359.2)
+  it('should test for the value of kilogramWeight', function() {
+    let conversion = new weightConversion (45.3592)
+    expect(conversion.kiloramWeight).toEqual(45.3592)
   });
 });

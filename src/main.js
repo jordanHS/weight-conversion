fff3f853b6b93097b400e18a8e15f91c0d6c847f
Student <example@example.com>
2019-09-22 16:01:37 -0700

    build main js code

--- src/main.js
+++ src/main.js
@@ -5,7 +5,31 @@ $(document).ready(function() {
   $("#convert").submit(function(event) {
     event.preventDefault();
  
-    let kilogramWeight = parseInt($("#userWeight").val());
-    let meterHeight = parseInt($("userHeight").val());
+    let kilogramWeight = parseFloat($("#userWeight").val());
+    let meterHeight = parseFloat($("userHeight").val());
+  
+    let conversion = new weightConversion(kilogramWeight, meterHeight, gramWeight, poundWeight, libraWeight, funtWeight, changWeight, rotalWeight);
+
+    let gramWeight = conversion.gramWeight(kilogramWeight, multiplier);
+    let poundWeight = conversion.poundWeight(kilogramWeight, multiplier);
+    let libraWeight = conversion.libraWeight(gramWeight, multiplier);
+    let funtWeight = conversion.funtWeight(gramWeight, multiplier);
+    let changWeight = conversion.changWeight(kilogramWeight, multiplier);
+    let rotalWeight = conversion.rotalWeight(rotalWeight, multiplier);
+    
+    $(".conversion").text(gramWeight);
+    $(".conversion").text(poundWeight);
+    $(".conversion").text(libraWeight);
+    $(".conversion").text(funtWeight);
+    $(".conversion").text(changWeight);
+    $(".conversion").text(rotalWeight);
+
+
+    
+  
+  
+  
+  
+  
   });
 });
